7de8760629bd0b4b88ae2bf0ce8caf4c79e9ed51
leidyolinto <suverleide.olinto@gmail.com>
2024-05-02 01:36:21 -0300

    feat: add secound promo banner to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Search from "./_components/search";
 import Header from "./_components/header";
 import CategoryList from "./_components/category-list";
-import Image from "next/image";
 import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/ui/promo-banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -34,27 +34,29 @@ const Home = async () => {
         <CategoryList />
       </div>
       <div className="px-5 pt-6">
-        <Image
+        <PromoBanner
           src="/promo-banner-01.png"
           alt="Até 30% de desconto em pizza"
-          height={0}
-          width={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
         />
       </div>
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
-        <h2 className="font-semibold">Pedidos Recomendados</h2>
-        <Button variant="ghost" className="h-fit p-0 text-primary hover:bg-transparent">
-          Ver todos
-          <ChevronRightIcon size={16} />
-        </Button>
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button variant="ghost" className="h-fit p-0 text-primary hover:bg-transparent">
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
         </div>
         <ProductList products={products} />
       </div>
-     
+      <div className="div px-5 pt-6">
+        <PromoBanner
+        src="/promo-banner-02.png"
+        alt="A partir de R$17,90 em lanches"
+        />
+
+      </div>
+
     </>
 
   )
